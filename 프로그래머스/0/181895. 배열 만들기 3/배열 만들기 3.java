bf3a24cae44f88e55a18dc2857441e6f1fd0dd1b
choiyuran <133099209+choiyuran@users.noreply.github.com>
2024-03-19 20:50:12 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.10 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        int size = (intervals[0][1] - intervals[0][0] + 1) + (intervals[1][1] - intervals[1][0] + 1);
+        int[] answer = new int[size];
+        int index = 0;
+        for(int i = intervals[0][0]; i <= intervals[0][1]; i++) {
+            answer[index++] = arr[i];
+        }
+        for(int i = intervals[1][0]; i <= intervals[1][1]; i++) {
+            answer[index++] = arr[i];
+        }
+        
+        return answer;
+    }
+}
