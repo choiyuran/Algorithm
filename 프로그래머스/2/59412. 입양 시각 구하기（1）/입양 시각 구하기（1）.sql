28fae640053c0aa7ff996d67c691075584318991
choiyuran <133099209+choiyuran@users.noreply.github.com>
2024-04-26 15:09:52 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,5 +1,5 @@
-SELECT TO_NUMBER(TO_CHAR(datetime, 'hh24')) AS hour, COUNT(animal_id) AS count
+SELECT TO_NUMBER(TO_CHAR(datetime, 'HH24')) AS hour, COUNT(animal_id) AS count
     FROM animal_outs
-    WHERE TO_NUMBER(TO_CHAR(datetime, 'hh24')) between '09' and '19'
-    GROUP BY TO_NUMBER(TO_CHAR(datetime, 'hh24'))
-    ORDER BY TO_NUMBER(TO_CHAR(datetime, 'hh24'));
+    WHERE TO_NUMBER(TO_CHAR(datetime, 'HH24')) between '09' and '19'
+    GROUP BY TO_NUMBER(TO_CHAR(datetime, 'HH24'))
+    ORDER BY TO_NUMBER(TO_CHAR(datetime, 'HH24'));
