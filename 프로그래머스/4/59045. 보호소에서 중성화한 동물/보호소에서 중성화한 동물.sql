f4b35ad6a5024a20adfa0d9f9c94000ac46159b8
choiyuran <133099209+choiyuran@users.noreply.github.com>
2024-05-03 17:37:24 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,7 @@
+SELECT animal_id, animal_type, name
+    FROM animal_outs 
+    WHERE (sex_upon_outcome like 'Spayed%' OR sex_upon_outcome like 'Neutered%') 
+    AND animal_id IN (SELECT animal_id
+    FROM animal_ins WHERE sex_upon_intake like 'intact%') 
+    ORDER BY animal_id;
+    
