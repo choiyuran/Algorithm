58c8b1083e85de2f707e50686b6219e9ba6194f3
choiyuran <133099209+choiyuran@users.noreply.github.com>
2024-05-09 23:12:03 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,8 +1,5 @@
 SELECT id, email, first_name, last_name
     FROM developers 
-    WHERE skill_code & (
-    SELECT sum(code)
-    FROM skillcodes
-    WHERE category = 'Front End'
+    WHERE skill_code & (SELECT sum(code) FROM skillcodes WHERE category = 'Front End'
     group by category)
 ORDER BY id;
