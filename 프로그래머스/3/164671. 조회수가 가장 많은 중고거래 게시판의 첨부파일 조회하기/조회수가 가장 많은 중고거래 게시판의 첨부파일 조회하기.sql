452f62f05f4953464e68926c7f666aa0db1e8aad
choiyuran <133099209+choiyuran@users.noreply.github.com>
2024-05-10 18:16:41 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,9 @@
+SELECT CONCAT('/home/grep/src/', f.board_id, '/', file_id, file_name, file_ext) AS file_path
+    FROM used_goods_file f, used_goods_board b
+    WHERE f.board_id = b.board_id AND b.views IN (
+        SELECT MAX(views) FROM used_goods_board)
+    ORDER BY file_id DESC;
+
+    
+    
+ 
