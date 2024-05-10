00893da1a10197246d7230c6dc6fad4632340512
choiyuran <133099209+choiyuran@users.noreply.github.com>
2024-05-10 17:23:29 +0900

    [level 2] Title: 이름에 el이 들어가는 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++ 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -1,4 +1,5 @@
 SELECT animal_id, name
     FROM animal_ins
-    WHERE name like '%el%' AND animal_type = 'Dog'
+    WHERE (name like '%el%' OR name like '%El%')
+    AND animal_type = 'Dog'
     ORDER BY name;
