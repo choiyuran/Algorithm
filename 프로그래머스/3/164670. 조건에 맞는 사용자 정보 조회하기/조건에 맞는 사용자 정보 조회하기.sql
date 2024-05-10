f2d724059cdb8f9d5b1ae7c8acba19be3366f048
choiyuran <133099209+choiyuran@users.noreply.github.com>
2024-05-10 22:13:45 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,14 @@
+SELECT user_id, nickname,
+    city || ' ' || street_address1 || ' ' || street_address2 AS 전체주소,
+    SUBSTR(tlno, 1, 3) || '-' || SUBSTR(tlno, 4, 4) || '-' || SUBSTR(tlno, 8, 4) AS 전화번호
+    FROM used_goods_user u
+    JOIN (SELECT writer_id, COUNT(writer_id)
+                     FROM used_goods_board 
+                     GROUP BY writer_id
+                     HAVING COUNT(writer_id) >= 3) result
+    ON u.user_id = result.writer_id
+    ORDER BY user_id DESC;
+
+
+
+    
