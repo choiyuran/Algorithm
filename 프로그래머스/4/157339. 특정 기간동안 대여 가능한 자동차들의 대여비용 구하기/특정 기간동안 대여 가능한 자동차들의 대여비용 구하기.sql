292b06297a01b3fd09be74725a20e71994433f9d
choiyuran <133099209+choiyuran@users.noreply.github.com>
2024-05-21 16:05:09 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -5,9 +5,11 @@ JOIN car_rental_company_rental_history h ON c.car_id = h.car_id
 JOIN car_rental_company_discount_plan p ON c.car_type = p.car_type
 WHERE c.car_id NOT IN (
     SELECT car_id FROM car_rental_company_rental_history
-    WHERE end_date > '2022-11-01' AND start_date < '2022-12-01')
+    WHERE end_date > TO_DATE('2022-11-01', 'YYYY-MM-DD') 
+    AND start_date < TO_DATE('2022-12-01', 'YYYY-MM-DD'))
 AND p.duration_type = '30일 이상'
 AND c.car_type IN ('세단', 'SUV')
-GROUP BY c.car_id, c.car_type, fee
-HAVING fee >= 500000 AND fee < 2000000
+GROUP BY c.car_id, c.car_type, c.daily_fee, p.discount_rate
+HAVING ROUND(c.daily_fee * 30 * (100 - p.discount_rate) / 100) >= 500000 
+   AND ROUND(c.daily_fee * 30 * (100 - p.discount_rate) / 100) < 2000000
 ORDER BY fee DESC, c.car_type, c.car_id DESC;
