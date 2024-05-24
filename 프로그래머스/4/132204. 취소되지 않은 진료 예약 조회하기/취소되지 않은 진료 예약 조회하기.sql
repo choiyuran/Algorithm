48ff249975121b0838f7dc882a87c3ab6a6d3982
choiyuran <133099209+choiyuran@users.noreply.github.com>
2024-05-24 15:40:49 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -2,7 +2,7 @@ SELECT a.apnt_no, p.pt_name, p.pt_no, a.mcdp_cd, d.dr_name, a.apnt_ymd
     FROM appointment a
     JOIN doctor d ON a.mddr_id = d.dr_id
     JOIN patient p ON a.pt_no = p.pt_no
-    WHERE (MONTH(apnt_ymd) = '04' AND DAY(apnt_ymd) = '13')
+    WHERE (TO_CHAR(apnt_ymd, 'MM-dd') = '04-13')
     AND apnt_cncl_yn = 'N'
     ORDER BY apnt_ymd;
 
