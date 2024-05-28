4a3ee798a20a54888322ef80e8901e64abb95cd5
choiyuran <133099209+choiyuran@users.noreply.github.com>
2024-05-28 13:22:47 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -0,0 +1,14 @@
+WITH A AS (
+    SELECT cart_id
+    FROM cart_products
+    WHERE name = 'Milk'
+),
+B AS (
+    SELECT cart_id
+    FROM cart_products
+    WHERE name = 'Yogurt'
+)
+
+
+SELECT A.cart_id 
+    FROM A JOIN B ON A.cart_id = B.cart_id;
