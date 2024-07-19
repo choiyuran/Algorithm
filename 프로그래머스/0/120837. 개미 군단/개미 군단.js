08bdcdff7b522ee0909c130dd38053c871a6bdd1
choiyuran <133099209+choiyuran@users.noreply.github.com>
2024-07-19 15:22:58 +0900

    [level 0] Title: 개미 군단, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,9 @@
+function solution(hp) {
+    var answer = 0;
+    answer += Math.floor(hp / 5);
+    hp = Math.floor(hp % 5);
+    answer += Math.floor(hp / 3);
+    hp = Math.floor(hp % 3);
+    answer += hp;
+    return answer;
+}
